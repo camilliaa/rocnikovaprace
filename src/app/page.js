f07f02fc6d22fbce3e8ca3cf327f42d05fce62c5
JanStork <jan.stork@protonmail.com>
2024-12-23 01:09:52 +0100

    vgew

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,22 @@ import '../styles/page.css';
 import BGVid from '../img/background_video.mp4';
 
 export default function MainContent() {
-  return (
-      <main>
-          <div>
-              <img className='bg' src="/img/bgb.png" alt='backup-background' z-index="-2"/>
-              <video className='video' autoPlay loop muted playsInline preload='auto'>
-                  <source src={BGVid} type='video/mp4' />
-              </video>
-          </div>
-          <div className="introduction">
-              <h1>Dí Rezo Daněk</h1>
-              <p>tatér od roku 2013</p>
-              <p>tetování Znojmo & Brno</p>
-              <Link href='/booking' className="booking-button">
+    return (
+        <main>
+            <div>
+                <img className='bg' src="/img/bgb.png" alt='backup-background' z-index="-2"/>
+                <video className='video' autoPlay loop muted playsInline preload='auto'>
+                    <source src={BGVid} type='video/mp4' />
+                </video>
+            </div>
+            <div className="introduction">
+                <h1>Dí Rezo Daněk</h1>
+                <p>tatér od roku 2013</p>
+                <p>tetování Znojmo & Brno</p>
+                <Link href='/booking' className="booking-button">
                 Booking
-              </Link>
-          </div>
-      </main>
-  );
+                </Link>
+            </div>
+    </main>
+);
 }
