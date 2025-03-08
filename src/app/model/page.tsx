e34c615c753402f9877f5734edcb7e3b358d0f76
JanStork <jan.stork@protonmail.com>
2025-03-08 18:21:48 +0100

    jestli neprojdu tak se předávkuju na hajzlech

--- src/app/model/page.tsx
+++ src/app/model/page.tsx
@@ -2,11 +2,23 @@
 import React, { useState } from 'react';
 import TattooPositioner from '@/components/TattooPositioner/TattooPositioner';
 
-export default function Page() { 
-
+export default function Page() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  
+  const [scale, setScale] = useState(1);
+  const [rotation, setRotation] = useState(0);
+  const [baseImage, setBaseImage] = useState(null);
+  const [tattooImage, setTattooImage] = useState(null);
+
   return (
-    <TattooPositioner position={position} setPosition={setPosition} />
+    <TattooPositioner
+      position={position}
+      setPosition={setPosition}
+      scale={scale}
+      setScale={setScale}
+      rotation={rotation}
+      setRotation={setRotation}
+      baseImage={baseImage}
+      tattooImage={tattooImage}
+    />
   );
 }
